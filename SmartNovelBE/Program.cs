35341ecccbf69661779b2848bd56f27e05caf7a0
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-05-22 23:50:56 +0700

    test api

--- SmartNovelBE/Program.cs
+++ SmartNovelBE/Program.cs
@@ -1,3 +1,5 @@
+using SmartNovelBE.Models;
+using Microsoft.EntityFrameworkCore;    
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+var conString = builder.Configuration.GetConnectionString("SmartNovel");
+builder.Services.AddDbContext<SmartTruyenDbContext>(options =>
+    options.UseSqlServer(conString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

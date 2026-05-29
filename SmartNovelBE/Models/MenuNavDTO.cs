dd119b57226c4575eb5c318bc13e6c4140dfbd18
HaruAlex <yn84258@gmail.com>
2026-05-29 12:53:40 +0700

    header

---
+++ SmartNovelBE/Models/MenuNavDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartNovelBE.Models
+{
+    public class MenuNavDTO
+    {
+        public int ID { get; set; }
+
+        public string? IconBootstrap { get; set; }
+
+        public string Content { get; set; } = null!;
+
+        public string? UrlLink { get; set; }
+
+        public int? Slots { get; set; }
+
+        public List<MenuNavDTO> Children { get; set; } = new();
+    }
+}

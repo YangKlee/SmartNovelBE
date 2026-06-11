edc4c01855d7feb6c6ac584390ee763c085d4a49
Yuki <votrantrangchau@gmail.com>
2026-06-11 21:44:05 +0700

    https://localhost:7134/api/AuthorList

---
+++ SmartNovelBE/Models/AuthorListModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartNovelBE.Models
+{
+    public class AuthorListModel
+    {
+        public string? Keyword { get; set; }
+
+        public int CurrentPage { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public int TotalItems { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public List<object> Authors { get; set; } = new();
+    }
+}
